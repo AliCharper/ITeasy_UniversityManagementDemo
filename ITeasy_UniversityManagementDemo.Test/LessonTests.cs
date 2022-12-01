57d1fceedd300ade4cce8a1755b0a7f64658d37e
AliCharper <alikolahdoozan@gmail.com>
2022-12-01 21:32:41 +0800

    Boolean Assertation

---
+++ ITeasy_UniversityManagementDemo.Test/LessonTests.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITeasy_UniversityManagementDemo.Test
+{
+    public class LessonTests
+    {
+        [Fact]
+        public void LessonConstructor_ConstructLesson_IsNewMustBeTrue()
+        {
+            // Arrange
+            // nothing to see here
+
+            // Act
+            var lesson = new Lesson("C#.NET 11 - Advanced");
+
+            // Assert
+            Assert.False(lesson.IsNew);
+        }
+    }
+}
